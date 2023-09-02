30f39625b5cc8cbb793ad1831321ce0f56303ba0
anmolsinghsahi <anmol.sahi@mutualmobile.com>
2023-09-02 12:45:26 +0530

    complete the home screen

--- app/src/main/java/com/bestway/carpool/screens/homescreen/Home.kt
+++ app/src/main/java/com/bestway/carpool/screens/homescreen/Home.kt
@@ -1,54 +1,100 @@
 package com.bestway.carpool.screens.homescreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bestway.carpool.screens.homescreen.bottomsheet.AvailableCarPoolBottomSheet
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(
     modifier: Modifier = Modifier,
 ) {
-    val localConfiguration = LocalConfiguration.current
 
-    val bottomSheetPeekHeight by rememberSaveable {
-        mutableIntStateOf(localConfiguration.screenHeightDp / 2)
+    var currentLocationValue by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var destinationLocationValue by rememberSaveable {
+        mutableStateOf("")
     }
 
     BottomSheetScaffold(
         sheetContent = {
             AvailableCarPoolBottomSheet()
-        },
-        sheetPeekHeight = bottomSheetPeekHeight.dp
-    ) {
-        val singapore = LatLng(1.35, 103.87)
-        val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 10f)
         }
-        GoogleMap(
-            modifier = modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
+    ) {
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
         ) {
-            Marker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                value = currentLocationValue,
+                label = {
+                    Text(
+                        text = "Enter you pickup",
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                onValueChange = { newValue ->
+                    currentLocationValue = newValue
+                },
+                singleLine = true,
             )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                value = destinationLocationValue,
+                label = {
+                    Text(
+                        text = "Enter your destination",
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                onValueChange = { newValue ->
+                    destinationLocationValue = newValue
+                },
+                singleLine = true
+            )
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Find CarPools")
+            }
+
+            Spacer(modifier = Modifier.weight(1.5f))
         }
     }
 }
